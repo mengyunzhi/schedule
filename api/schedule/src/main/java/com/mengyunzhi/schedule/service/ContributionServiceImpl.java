8a5b39aa14ee6785dc2fb89eee958bf73b4be10d
liyiheng1111 <2287217556@qq.com>
2018-11-24 10:52:24 +0800

    解决冲突

--- api/schedule/src/main/java/com/mengyunzhi/schedule/service/ContributionServiceImpl.java
+++ api/schedule/src/main/java/com/mengyunzhi/schedule/service/ContributionServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import java.awt.print.Pageable;
 import java.util.Date;
 import java.util.List;
 
