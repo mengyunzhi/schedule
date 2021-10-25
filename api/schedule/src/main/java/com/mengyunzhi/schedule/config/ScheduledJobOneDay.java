b761973a3d29af88063832887d4197e7516ade23
teacherpan <3792535@qq.com>
2021-10-25 22:29:23 +0800

    change to 22:00

--- api/schedule/src/main/java/com/mengyunzhi/schedule/config/ScheduledJobOneDay.java
+++ api/schedule/src/main/java/com/mengyunzhi/schedule/config/ScheduledJobOneDay.java
@@ -27,7 +27,7 @@ public class ScheduledJobOneDay{
     /**
      * 每晚22点触发
      */
-    @Scheduled(cron="0 25 22 * * ?")  //定时任务注解
+    @Scheduled(cron="0 0 22 * * ?")  //定时任务注解
     public void tomorrow()  {
         logger.info("发送信息");
         Calendar calendar = Calendar.getInstance();
